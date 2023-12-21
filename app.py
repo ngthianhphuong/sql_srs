db4c79e8323e93b69a19196e9858f7c4b4ed5f93
phuongborg <ngthianhphuong@gmail.com>
2023-12-21 16:50:48 +0100

    colleague selectbox minor fix - make branch up to date

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import duckdb
 st.title("# SQL SRS Spaced Repetition System SQL Practice")
 st.write("Hello world")
 
-with st.sidebar:
-    option = st.selectbox(
+
+option = st.selectbox(
         'What would you like to review?',
         ['Join', 'GroupBy', 'Windows Functions'],
         index=None,
         placeholder="Select a theme...",
     )
-    st.write("You've selected: ", option)
+st.write("You've selected: ", option)
 
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
