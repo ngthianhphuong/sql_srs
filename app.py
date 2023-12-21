8ac08dc5aa539837d8c826f1e330de3751140053
phuongborg <ngthianhphuong@gmail.com>
2023-12-21 14:12:14 +0100

    add sidebar

--- app.py
+++ app.py
@@ -7,18 +7,17 @@ import duckdb
 st.title("# SQL SRS Spaced Repetition System SQL Practice")
 st.write("Hello world")
 
-option = st.selectbox(
-    'What would you like to review?',
-    ['Join', 'GroupBy', 'Windows Functions'],
-    index=None,
-    placeholder="Select a theme...",
-)
-
-st.write("You've selected: ", option)
+with st.sidebar:
+    option = st.selectbox(
+        'What would you like to review?',
+        ['Join', 'GroupBy', 'Windows Functions'],
+        index=None,
+        placeholder="Select a theme...",
+    )
+    st.write("You've selected: ", option)
 
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
-
 
 tab1, tab2, tab3, tab4 = st.tabs(["Recherche", "Cat", "Dog", "Owl"])
 
