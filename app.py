d5570b1a102438a8bb5ee69e79634ba63a9a6f69
phuongborg <ngthianhphuong@gmail.com>
2024-01-02 10:57:35 +0100

    colleague add sql question

--- app.py
+++ app.py
@@ -2,46 +2,56 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import duckdb
-
+import io
 
 st.title("# SQL SRS Spaced Repetition System SQL Practice")
-st.write("Hello world")
 
-option = st.selectbox(
-    'What would you like to review?',
-    ['Join', 'GroupBy', 'Windows Functions'],
-    index=None,
-    placeholder="Select a theme...",
-)
+csv = """
+beverage, price
+orange juice, 2.5
+expresso, 2 
+tea, 3
+"""
 
-st.write("You've selected: ", option)
+beverages = pd.read_csv(io.StringIO(csv))
 
-data = {"a": [1, 2, 3], "b": [4, 5, 6]}
-df = pd.DataFrame(data)
+csv2 = """
+food-item, food_price
+cookie juice, 2.5
+chocolatine, 2
+muffin, 3
+"""
 
+food_items = pd.read_csv(io.StringIO(csv2))
 
-tab1, tab2, tab3, tab4 = st.tabs(["Recherche", "Cat", "Dog", "Owl"])
+# la réponse que l'utilisateur doit donner
+answer = """
+SELECT * FROM beverages
+CROSS JOIN 
+food_items"""
+
+# la solution = la dataframe créée à partir de la requête SQL
+solution = duckdb.sql(answer).df()
+
+st.header("Enter your code:")
+query = st.text_area(label="Votre code SQL ici", key="user_input")
+if query:
+    # l'output de la requête de l'utilisateur
+    result = duckdb.sql(query).df()
+    st.dataframe(result)
+
+tab1, tab2 = st.tabs(["Tables", "Solution"])
 
 with tab1:
-    sql_query = st.text_area(label="Entrez votre input")
-    st.write("Vous avez entré la query suivante: " + str(sql_query))
-    st.dataframe(duckdb.sql(sql_query).df())
-
-
-   #result = duckdb.sql(sql_query).df()
-   #st.write(f"Vous avez entré la query suivante: {sql_query}")
-   #st.dataframe(result)
+    st.write("Table: beverages")
+    st.dataframe(beverages)
+    st.write("Table: food_items")
+    st.dataframe(food_items)
+    st.write("Expected output:")
+    st.dataframe(solution)
 
 with tab2:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    st.write(answer)
 
-with tab3:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-with tab4:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 
